1d7226cac5450afb68b6247a540e2ce862f3d748
Malii61 <031990031@ogr.uludag.edu.tr>
2022-05-09 17:51:19 +0300

    yeni

---
+++ Assets/Manager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System; 
+public class Manager : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameObject go1 = new GameObject();
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Console.Write("Annen evde mi?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Console.Write("Annen evde mi?");
+
+    }
+}
