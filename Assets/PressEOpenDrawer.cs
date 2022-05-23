9ac11244538b689e18615f6d6ebd7d1fc7a7472d
Malii61 <031990031@ogr.uludag.edu.tr>
2022-05-24 02:51:30 +0300

    çekmece

---
+++ Assets/PressEOpenDrawer.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressEOpenDrawer : MonoBehaviour
+{
+    public GameObject Instruction;
+    public GameObject AnimeObject;
+    public bool Action = false;
+    private bool opened = false;
+    public Animator anim;
+    private float animTime = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instruction.GetComponent<MeshRenderer>().enabled = true;
+        Instruction.SetActive(false);
+    }
+    void OnTriggerStay(Collider collison)
+    {
+        if (collison.transform.tag == "Player")
+        {
+            Instruction.SetActive(true);
+            Action = true;
+        }
+    }
+    void OnTriggerExit(Collider collision)
+    {
+        Instruction?.SetActive(false);
+        Action = false;
+    }
+    void Pressed()
+    {
+        opened = !opened;
+        anim.SetBool("Opened", !opened);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        animTime += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            if (Action == true)
+            {
+                
+                    Instruction.SetActive(false);
+                    if (animTime >= 1f)
+                    {
+                        AnimeObject.GetComponent<Animator>().Play("DrawerOpen");
+                        Pressed();
+                        animTime = 0f;
+                    }
+                    Action = false;
+                
+                
+
+
+            }
+        }
+    }
+}
